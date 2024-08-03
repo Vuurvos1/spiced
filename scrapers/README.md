651f1bcac00886a93273be18cf0eb9210529c145
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2024-08-04 00:27:30 +0200

    restructure scraper

---
+++ scrapers/README.md
@@ -0,0 +1,17 @@
+# Scrapers
+
+- heatsupply
+- trex hot sauce `trex`
+- chilisaus be
+- heatonist
+
+## Running a scraper
+
+```bash
+node index.js trex --noCache # --noCache to re-fetch all products
+```
+
+## TODO
+
+- [ ] Add more scrapers
+- [ ] Write to db
--- scrapers/index.d.ts
+++ scrapers/index.d.ts
@@ -9,6 +9,7 @@ export type Sauce = {
 
 // Could be a class with method chaining?
 export type SauceScraper = {
+  baseUrl: string;
   getSauceUrls: (url: string, cache: boolean) => Promise<string[]>;
   scrapeSauces: (sauceUrls: string[], cache: boolean) => Promise<Sauce[]>;
 };
