eeca9667be43947e7fb4f21c77a23ef71565412a
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2024-08-04 23:45:58 +0200

    reviews and stuff

---
+++ apps/site/src/routes/Header.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<header class="sticky top-0">
+	<nav class="border-b bg-white py-4">
+		<ul class="container flex flex-row gap-4">
+			<li>
+				<a href="/sauces">sauces</a>
+			</li>
+		</ul>
+	</nav>
+</header>
--- apps/site/src/routes/sauces/+page.server.ts
+++ apps/site/src/routes/sauces/+page.server.ts
@@ -1,3 +1,18 @@
-export async function load() {
-	return {};
+import { db } from '$lib/db';
+import { hotSauces } from '@app/db/schema';
+import { count, desc } from 'drizzle-orm';
+
+export async function load({ url }) {
+	const page = Number(url.searchParams.get('page')) || 0;
+	const pageSize = 24;
+
+	const sauceCount = await db.select({ count: count() }).from(hotSauces);
+	const sauces = await db
+		.select()
+		.from(hotSauces)
+		.orderBy(desc(hotSauces.createdAt))
+		.limit(pageSize)
+		.offset(page * pageSize);
+
+	return { sauces, sauceCount: sauceCount[0].count, pageSize };
 }
