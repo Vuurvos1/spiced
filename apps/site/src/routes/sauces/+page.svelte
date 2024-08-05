cf7ecb5f8f23cab795a757eea2f4d850bec81a91
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2024-08-05 23:51:45 +0200

    searching stuff

--- apps/site/src/routes/sauces/+page.svelte
+++ apps/site/src/routes/sauces/+page.svelte
@@ -7,19 +7,41 @@
 	let { sauces, sauceCount, pageSize } = $derived(data);
 
 	const currentPage = $derived(Math.max(Number($page.url.searchParams.get('page')) || 1, 1));
+
+	let search = $state($page.url.searchParams.get('search') || '');
+
+	const previousUrl = $derived.by(() => {
+		const url = new URL($page.url);
+		url.searchParams.set('page', String(currentPage - 1));
+		return url.toString();
+	});
+
+	const nextUrl = $derived.by(() => {
+		const url = new URL($page.url);
+		url.searchParams.set('page', String(currentPage + 1));
+		return url.toString();
+	});
 </script>
 
 <div class="container">
-	<h1 class="text-4xl font-semibold">Sauces</h1>
+	<div class="flex flex-row items-center justify-between">
+		<div>
+			<h1 class="text-4xl font-semibold">Sauces</h1>
 
-	<p class="text-gray-500">Showing {sauceCount} sauces</p>
+			<p class="text-gray-500">Showing {sauceCount} sauces</p>
+		</div>
+
+		<form data-sveltekit-keepfocus>
+			<input bind:value={search} placeholder="Search" type="text" name="search" />
+		</form>
+	</div>
 
 	<SauceGrid {sauces}></SauceGrid>
 
 	<div class="flex flex-row justify-end gap-6 py-4">
 		<a
-			href={`/sauces?page=${currentPage - 1}`}
-			class={`${currentPage < 2 ? 'pointer-events-none cursor-not-allowed opacity-70' : ''}`}
+			href={previousUrl}
+			class={currentPage < 2 ? 'pointer-events-none cursor-not-allowed opacity-70' : ''}
 		>
 			Prev
 		</a>
@@ -29,8 +51,10 @@
 		</span>
 
 		<a
-			href={`/sauces?page=${currentPage + 1}`}
-			class={`${currentPage > Math.ceil(sauceCount / pageSize) - 1 ? 'pointer-events-none cursor-not-allowed opacity-70' : ''}`}
+			href={nextUrl}
+			class={currentPage > Math.ceil(sauceCount / pageSize) - 1
+				? 'pointer-events-none cursor-not-allowed opacity-70'
+				: ''}
 		>
 			Next
 		</a>
