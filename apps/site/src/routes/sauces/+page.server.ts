5c5f29c63123d21b3c4c1873e39d1c9611c0cbba
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2024-08-05 23:51:45 +0200

    searching stuff

--- apps/site/src/routes/sauces/+page.server.ts
+++ apps/site/src/routes/sauces/+page.server.ts
@@ -1,15 +1,22 @@
 import { db } from '$lib/db';
 import { hotSauces } from '@app/db/schema';
-import { count, desc } from 'drizzle-orm';
+import { count, desc, ilike } from 'drizzle-orm';
 
 export async function load({ url }) {
 	const page = Number(url.searchParams.get('page')) || 1;
 	const pageSize = 24;
 
-	const sauceCount = await db.select({ count: count() }).from(hotSauces);
+	const search = url.searchParams.get('search') ?? '';
+
+	const sauceCount = await db
+		.select({ count: count() })
+		.from(hotSauces)
+		.where(ilike(hotSauces.name, `%${search}%`));
+
 	const sauces = await db
 		.select()
 		.from(hotSauces)
+		.where(ilike(hotSauces.name, `%${search}%`)) // No sql injection here, I checked :)
 		.orderBy(desc(hotSauces.createdAt))
 		.limit(pageSize)
 		.offset((page - 1) * pageSize);
