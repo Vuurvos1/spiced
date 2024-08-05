bf9b7ff70bbb05b223f165845243773dcc65bdb0
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2024-08-05 23:08:39 +0200

    make sauce pages based

--- apps/site/src/routes/sauces/+page.server.ts
+++ apps/site/src/routes/sauces/+page.server.ts
@@ -3,7 +3,7 @@ import { hotSauces } from '@app/db/schema';
 import { count, desc } from 'drizzle-orm';
 
 export async function load({ url }) {
-	const page = Number(url.searchParams.get('page')) || 0;
+	const page = Number(url.searchParams.get('page')) || 1;
 	const pageSize = 24;
 
 	const sauceCount = await db.select({ count: count() }).from(hotSauces);
@@ -12,7 +12,7 @@ export async function load({ url }) {
 		.from(hotSauces)
 		.orderBy(desc(hotSauces.createdAt))
 		.limit(pageSize)
-		.offset(page * pageSize);
+		.offset((page - 1) * pageSize);
 
 	return { sauces, sauceCount: sauceCount[0].count, pageSize };
 }
