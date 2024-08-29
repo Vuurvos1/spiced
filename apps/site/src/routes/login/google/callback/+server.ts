e882de6c95288f39881f9e87478368ec53d54e10
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2024-08-29 22:47:16 +0200

    wip: adding google oauth

---
+++ apps/site/src/routes/login/google/callback/+server.ts
@@ -0,0 +1,78 @@
+import { OAuth2RequestError } from 'arctic';
+import { generateIdFromEntropySize } from 'lucia';
+import { google, lucia } from '$lib/server/lucia';
+
+import type { RequestEvent } from '@sveltejs/kit';
+
+export async function GET(event: RequestEvent): Promise<Response> {
+	const code = event.url.searchParams.get('code');
+	const state = event.url.searchParams.get('state');
+	const storedState = event.cookies.get('github_oauth_state') ?? null;
+	const codeVerifier = event.cookies.get('codeVerifier') ?? null;
+
+	if (!code || !state || !storedState || !codeVerifier || state !== storedState) {
+		return new Response(null, {
+			status: 400
+		});
+	}
+
+	try {
+		const tokens = await google.validateAuthorizationCode(code, codeVerifier);
+		const githubUserResponse = await fetch('https://googleapis.com/oath2/v1/userinfo', {
+			headers: {
+				Authorization: `Bearer ${tokens.accessToken}`
+			}
+		});
+		const githubUser: GitHubUser = await githubUserResponse.json();
+
+		// Replace this with your own DB client.
+		const existingUser = await db.table('user').where('github_id', '=', githubUser.id).get();
+
+		if (existingUser) {
+			const session = await lucia.createSession(existingUser.id, {});
+			const sessionCookie = lucia.createSessionCookie(session.id);
+			event.cookies.set(sessionCookie.name, sessionCookie.value, {
+				path: '.',
+				...sessionCookie.attributes
+			});
+		} else {
+			const userId = generateIdFromEntropySize(10); // 16 characters long
+
+			// Replace this with your own DB client.
+			await db.table('user').insert({
+				id: userId,
+				github_id: githubUser.id,
+				username: githubUser.login
+			});
+
+			const session = await lucia.createSession(userId, {});
+			const sessionCookie = lucia.createSessionCookie(session.id);
+			event.cookies.set(sessionCookie.name, sessionCookie.value, {
+				path: '.',
+				...sessionCookie.attributes
+			});
+		}
+		return new Response(null, {
+			status: 302,
+			headers: {
+				Location: '/'
+			}
+		});
+	} catch (e) {
+		// the specific error message depends on the provider
+		if (e instanceof OAuth2RequestError) {
+			// invalid code
+			return new Response(null, {
+				status: 400
+			});
+		}
+		return new Response(null, {
+			status: 500
+		});
+	}
+}
+
+interface GitHubUser {
+	id: number;
+	login: string;
+}
