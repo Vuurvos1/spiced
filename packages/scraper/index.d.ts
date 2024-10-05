96758515cc8b36e1ec7916ea0355de959a94c4c8
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2024-10-05 10:08:55 +0200

    thinking about stores and makers

--- packages/scraper/index.d.ts
+++ packages/scraper/index.d.ts
@@ -3,15 +3,26 @@ import type { HotSauceInsert } from '@app/db/types';
 export type Sauce = HotSauceInsert;
 
 export type GetSauceUrls = (url: string, cache: boolean) => Promise<string[]>;
-export type ScrapeSauces = (
-  sauceUrls: string[],
-  cache: boolean
-) => Promise<HotSauceInsert[]>;
+export type ScrapeSauces = (sauceUrls: string[], cache: boolean) => Promise<HotSauceInsert[]>;
 
 // Could be a class with method chaining?
 export type SauceScraper = {
-  baseUrl: string;
-  name: string;
-  getSauceUrls: GetSauceUrls;
-  scrapeSauces: ScrapeSauces;
+	baseUrl: string;
+	name: string;
+	getSauceUrls: GetSauceUrls;
+	scrapeSauces: ScrapeSauces;
+};
+
+export type Maker = {
+	name: string;
+	description?: string;
+	url?: string;
+	logoUrl?: string;
+};
+
+export type Store = {
+	name: string;
+	description?: string;
+	url?: string;
+	logoUrl?: string;
 };
---
+++ packages/scraper/makers.js
@@ -0,0 +1,26 @@
+/** @type {import('./').Maker[]} */
+export const makers = [
+	{
+		name: 'T-rex Hot sauce',
+		url: 'https://trexhotsauce.com/',
+		description:
+			'T-rex Hot sauce is a Amsterdam based hot sauce brand. From our own little kitchen we make small batches of vegan and fermented hot sauce.',
+		logoUrl: 'https://t-rexhotsauce.com/cdn/shop/files/T-REX-LOGO-DESIGN-2024.png?v=1711643615'
+	},
+	{
+		name: 'Secret Aardvark Trading Co',
+		url: 'https://secretaardvark.com/',
+		logoUrl:
+			'https://mcusercontent.com/cf60fc50a4b715b2868f63b39/images/0e9d5723-9bff-594b-5979-17cd605058ef.png'
+	},
+	{
+		name: 'Nondedju',
+		url: 'https://nondedjuhetesaus.nl/en/',
+		logoUrl: 'https://nondedjuhetesaus.nl/wp-content/uploads/2022/11/Nondedju-Hete-Saus-logo.svg'
+	},
+	{
+		name: 'Piko Peppers',
+		url: 'https://microsauceriepikopeppers.com/',
+		description: `Whether you're looking at hot sauces, spicy jellies, or hotter-than-blazes condiments, all Piko Peppers products are homemade, in small batches, with fresh and quality ingredients.`
+	}
+];
