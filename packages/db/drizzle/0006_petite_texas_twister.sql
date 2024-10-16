e8917d517108291290f9ba4730c2d5f210fdaa05
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2024-10-16 22:06:49 +0200

    regenerate migrations

--- packages/db/drizzle/0006_petite_texas_twister.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "checkins" DROP CONSTRAINT "checkins_user_id_hot_sauce_id_unique";--> statement-breakpoint
-ALTER TABLE "wishlist" DROP CONSTRAINT "wishlist_user_id_hot_sauce_id_unique";--> statement-breakpoint
-ALTER TABLE "checkins" DROP COLUMN IF EXISTS "checkin_id";--> statement-breakpoint
-ALTER TABLE "wishlist" DROP COLUMN IF EXISTS "wishlist_id";
-ALTER TABLE "checkins" ADD CONSTRAINT "checkins_user_id_hot_sauce_id_pk" PRIMARY KEY("user_id","hot_sauce_id");--> statement-breakpoint
-ALTER TABLE "wishlist" ADD CONSTRAINT "wishlist_user_id_hot_sauce_id_pk" PRIMARY KEY("user_id","hot_sauce_id");--> statement-breakpoint
--- packages/db/drizzle/0008_talented_leopardon.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "checkins" ADD COLUMN "review" text DEFAULT '';--> statement-breakpoint
-ALTER TABLE "checkins" DROP COLUMN IF EXISTS "review_text";
