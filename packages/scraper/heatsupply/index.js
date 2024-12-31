9c0cf69f8636457a7ff8863f930a89a2c5e914a8
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2024-12-31 11:38:46 +0100

    chore: cleanup

--- packages/scraper/heatsupply/index.js
+++ packages/scraper/heatsupply/index.js
@@ -1,6 +1,5 @@
 import { JSDOM } from 'jsdom';
 import fs from 'node:fs';
-import chromium from '@sparticuz/chromium';
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker';
@@ -25,8 +24,6 @@ async function getSauceUrls(url, cache) {
 
 	// get amount of sauce pages
 	if (!fs.existsSync(`${cachePath}/saucePage.html`)) {
-		// executablePath: process.env.CHROME_EXECUTABLE_PATH ??
-		// 	'C:/Program Files/Google/Chrome/Application/chrome.exe';
 		const browser = await puppeteer.launch({});
 		const page = await browser.newPage();
 
